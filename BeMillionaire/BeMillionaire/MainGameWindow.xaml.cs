3519a85105db9cd0259e8d6641b43887986a5ba7
Georgiy Demenchuk <demenchuk.george@mail.ru>
2018-11-24 22:04:43 +0300

    Перезалив с использованием .gitignore, ура

---
+++ BeMillionaire/BeMillionaire/MainGameWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+
+namespace BeMillionaire
+{
+    /// <summary>
+    /// Логика взаимодействия для MainGameWindow.xaml
+    /// </summary>
+    public partial class MainGameWindow : Window
+    {
+        int thisnumber;
+        int globalcountnumber;
+        string localequestionscount;
+        public MainGameWindow(int countvalue, string allquestionscount)
+        {
+
+                InitializeComponent();
+                List<int> AllNumbers = RandomClass.GetList(false, 0);
+                this.Title = "Кто хочет стать миллионером? | Вопрос " + (countvalue + 1).ToString() + " из " + allquestionscount;
+                localequestionscount = allquestionscount;
+                globalcountnumber = countvalue;
+                thisnumber = AllNumbers[countvalue];
+                QuestionBlock.Text = SQLiteClass.SQLiteGet("SELECT question FROM Questions WHERE id=" + thisnumber.ToString());
+                FirstAnswerButton.Content = SQLiteClass.SQLiteGet("SELECT first FROM Questions WHERE id=" + thisnumber.ToString());
+                SecondAnswerButton.Content = SQLiteClass.SQLiteGet("SELECT second FROM Questions WHERE id=" + thisnumber.ToString());
+                ThirdAnswerButton.Content = SQLiteClass.SQLiteGet("SELECT third FROM Questions WHERE id=" + thisnumber.ToString());
+                ForthAnswerButton.Content = SQLiteClass.SQLiteGet("SELECT fourth FROM Questions WHERE id=" + thisnumber.ToString());
+            
+        }
+
+        private void Window_Initialized(object sender, System.EventArgs e)
+        {
+        }
+
+        private void CheckAnswer(int number)
+        {
+            string answerresult = SQLiteClass.SQLiteGet("SELECT answer FROM Questions WHERE id=" + thisnumber.ToString());
+            if (number.ToString() == answerresult)
+            {
+                MessageBox.Show("Вы выбрали правильный ответ!");
+                if ((globalcountnumber + 1) == System.Convert.ToInt32(localequestionscount))
+                {
+                    ResultWindow o = new ResultWindow(localequestionscount);
+                    o.Show();
+                }
+                else
+                {
+                    MainGameWindow newobj = new MainGameWindow(globalcountnumber + 1, localequestionscount);
+                    newobj.Show();
+                }
+                Close();
+            }
+            else
+            {
+                MessageBox.Show("Вы проиграли, хотите попробовать еще раз?");
+                Close();
+            }
+        }
+
+        private void FirstAnswerButton_Click(object sender, RoutedEventArgs e)
+        {
+            CheckAnswer(1);
+        }
+
+        private void SecondAnswerButton_Click(object sender, RoutedEventArgs e)
+        {
+            CheckAnswer(2);
+        }
+
+        private void ThirdAnswerButton_Click(object sender, RoutedEventArgs e)
+        {
+            CheckAnswer(3);
+        }
+
+        private void ForthAnswerButton_Click(object sender, RoutedEventArgs e)
+        {
+            CheckAnswer(4);
+        }
+    }
+}
---
+++ BeMillionaire/BeMillionaire/RandomClass.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeMillionaire
+{
+    class RandomClass
+    {
+        public static List<int> listNumbers;
+        public static List<int> GetList(bool newgeneration, int size)
+        {
+            if (newgeneration == true)
+            {
+                listNumbers = new List<int>();
+                Random rand = new Random();
+                int number;
+                for (int i = 0; i < size; i++)
+                {
+                    do
+                    {
+                        number = rand.Next(1, 10);
+                    } while (listNumbers.Contains(number));
+                    listNumbers.Add(number);
+                }
+            return listNumbers;
+            }
+            else
+            {
+                return listNumbers;
+            }
+        }
+    }
+}
