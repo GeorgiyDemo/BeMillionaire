49478d818b96ac9aee580e5d34c6f8c39b512ffa
Demka <16972045+GeorgiyDemo@users.noreply.github.com>
2021-12-25 00:34:16 +0300

    cleanup

---
+++ BeMillionaire/LoseWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+
+namespace BeMillionaire
+{
+    /// <summary>
+    /// Логика взаимодействия для LoseWindow.xaml
+    /// </summary>
+    public partial class LoseWindow : Window
+    {
+        public LoseWindow(int number, int price)
+        {
+            InitializeComponent();
+            LoseLabel.Content = "Вы проиграли!\nВы ответили на "+ number.ToString() + " из 15 вопросов c депозитом "+price.ToString()+ " руб.";
+        }
+
+        private void RepeatButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
